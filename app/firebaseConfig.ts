75f15cb30681577353ac150afeae4995065ffd6d
henr1q <carloshav44@gmail.com>
2025-05-02 21:41:19 -0300

    att

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Substitua o seguinte pela configuração do projeto Firebase do seu aplicativo.
 const firebaseConfig = {
-  apiKey: "AIzaSyDgMS88t34FSefGoMoL8YL8HGXhqKBYEWQ",
-  authDomain: "login-bd51d.firebaseapp.com",
-  projectId: "login-bd51d",
+  apiKey: "aqui",
+  authDomain: "aqui",
+  projectId: "aqui",
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "1:163310871460:android:2ed30b42232f1144291731",
+  appId: "aqui",
 };
 
 const app = initializeApp(firebaseConfig);
